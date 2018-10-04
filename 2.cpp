e2c7c5603e9d8bb382528e48996925a8903f6bda
RIAD-IIT7 <33634495+RIAD-IIT7@users.noreply.github.com>
2018-10-04 23:33:51 +0600

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,49 @@
+/*
+                                Bismillahi-r-Rahmani-r-Rahim
+                                        RhIeyAaD
+                                    IIT-7th Batch,JU
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll              long long int
+#define MAX             2134567891
+#define INF             1e9 + 7
+#define MIN             INT_MIN
+#define fr(i,n)         for(i=0;i<n;i++)
+#define rep(i,n)        for(i=1;i<=n;i++)
+#define ALL(n)          n.begin(),n.end()
+#define mem(x,n)        memset(x,n,sizeof(x))
+#define T_C(t)          printf("Case %lld: ",t)
+#define PAIR            pair<ll,ll>
+#define MP              make_pair
+#define pb(a)           push_back(a)
+#define ff              first
+#define ss              second
+#define MOD             1000000007///10^9
+#define p6              1000007 ///10^6->6 zero after 1 **
+#define hlw             printf("hlw\n");
+#define SN              4*p6
+#define NN              100
+#define N               p6
+
+ll x[N],y[N],z[N],n;
+string s,S;
+//vector<ll>v;
+//map <ll,ll> mp;
+
+int main()
+{
+    //while(1)
+    //READ;WRITE;
+{
+    ll a=0,b=0,c=0,d,diff,e,f,g,i,j,k,l,m,in,mod,loc,p,q,r,u,val,w,t,tc,sz,lo,hi,mid,mn=MAX,mx=0,sum=0,ans=0;
+
+
+}
+    return 0;
+}
+
+/*
+
+*/
